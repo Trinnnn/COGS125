4d6411d9cc6cbe729dc9fb9f52cb66b83f346688
Trinity Gao <ngao@ucsd.edu>
2024-05-02 11:42:43 -0700

    Add existing file

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import styles from "./App.module.css"
+import { Navbar } from "./componets/Navbar"
+
+function App() {
+
+  return <div className={styles.App}>
+    <Navbar/> </div>
+
+}
+
+export default App
